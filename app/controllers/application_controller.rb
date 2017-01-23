64068b4cf4be3fb2e00ebc9e3130f68bd28a6c8b
Vishal Antony <avishal@freshdesk.com>
2017-01-23 12:40:19 +0530

    Changed route and changed the greeting message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html:  "Hola, Mundos!"
+  	render html:  "Hello, World! My name is Vishal and this is my first RoR application! :-) "
   end
 
   def goodbye
   	render html: "Goodbye, World!"
   end
 end
+
+
